5be8acfbe9727cd7e86427b653093df8d5162fa9
Luis Vilches <lvilches21@gmail.com>
2022-06-10 13:02:10 -0400

    new feature format

--- services/format.ts
+++ services/format.ts
@@ -9,7 +9,18 @@ class FormatService {
       currency: currency,
     }).format(value);
   }
-}
 
+  queryParams(obj: Record<any, any>): string {
+    let query: string = "";
+
+    if (Object.keys(obj).length !== 0) {
+      query = `?${
+        Object.entries(obj).map(([key, val]) => `${key}=${val}`).join("&")
+      }`;
+    }
+
+    return query;
+  }
+}
 
 export default FormatService;
